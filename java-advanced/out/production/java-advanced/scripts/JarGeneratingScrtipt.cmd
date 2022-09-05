5490184c541cf58dbc8c89174dd877fb6514b74c
lukna5 <72575440+lukna5@users.noreply.github.com>
2022-09-05 23:04:38 +0500

    Delete java-advanced/out/production directory

--- java-advanced/out/production/java-advanced/scripts/JarGeneratingScrtipt.cmd
+++
@@ -1,6 +0,0 @@
-cd ..\java-solutions
-SET ModuleJar=..\..\java-advanced-2022\artifacts\info.kgeorgiy.java.advanced.implementor.jar
-SET IMPLEMENTOR=info\kgeorgiy\ja\kononov\implementor\Implementor.java
-SET ImplementorClass=info\kgeorgiy\ja\kononov\implementor\Implementor.class
-javac -cp %ModuleJar% %IMPLEMENTOR%
-jar cmvf MANIFEST.MF Implementor.jar %ImplementorClass%
